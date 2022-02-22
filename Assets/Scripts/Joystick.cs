584b36cef39c889b34a1b3058d3e6b78a9950488
sento26 <vicentegimenezmartinez@gmail.com>
2022-02-22 10:30:14 +0100

    Movimiento de imagen UI(Joystick)

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = GetComponent<RectTransform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 || Input.GetMouseButton(0))
+        {
+            GetComponent<RectTransform>().position = Input.mousePosition;
+            print(Input.mousePosition);
+            /*
+            Vector3 joy = this.transform.position;
+            print(joy);
+            Vector3 moveJoy = mousePos - joy;
+            //this.transform.position = moveJoy;
+            this.anchoredPosition = moveJoy;
+            */
+        }
+        else
+        {
+            GetComponent<RectTransform>().position = initialPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount >0 || Input.GetButton("Click_Raton"))
+        {
+            Vector3 movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+    }
+}
